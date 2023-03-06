401536733515c4c9d36d01945c5cad22b6832c02
Thomas Luijken <tluijken@outlook.com>
2023-03-06 10:55:29 +0100

    script did not work on arm based macbook, updated the following:
    1) added --platform linux/amd64 to docker build command in ./run-migrator.sh so that x86 images are pulled and zlib1g-dev and libmariadb3 installs end up in the expected place (where they are copied from)
    
    2) added --config net.git-fetch-with-client=tue flag to cargo to work around 137 error (oom), like described here https://github.com/rust-lang/cargo/issues/10781
    
    3) updated deprecated way of configuring external network (this was not strictly needed to get it working)

--- ivenza_auth_migrator/run-migrator.sh
+++ ivenza_auth_migrator/run-migrator.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 if [[ "$(docker images -q ivenza-auth-migrator:latest 2> /dev/null)" == "" ]]; then
     echo "Docker image not found, building..."
-    docker build -f Dockerfile . -t ivenza-auth-migrator
+    # added --platform linux/amd64 to docker build command in ./run-migrator.sh so that 
+    # x86 images are pulled and zlib1g-dev and libmariadb3 installs end up in the expected place 
+    # (where they are copied from)
+    docker build --platform linux/amd64 -f Dockerfile . -t ivenza-auth-migrator
 else
     echo "Docker image already found"
 fi
